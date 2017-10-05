08b553a644d7c405978cf1909ccf8438cedff4f9
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-10-05 10:44:30 -0700

    added find or create to controller

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -11,32 +11,69 @@ router.get('/recipes', function(req, res) {
     });
 });
 
-// POST - receive the name of a recipe and add it to the database
-router.post('/recipes', function(req, res) {
-  console.log('in the post to recipes favorite route ######');
-    // add to database, adding raw to a table
+// // POST - receive the name of a recipe and add it to the database
+// router.post('/favorites/show', function(req, res) {
+//   console.log('in the post to recipes favorite route ######');
+//     // add to database, adding raw to a table
+//
+//     var recipeTitle = req.body.title;
+//     db.favorite.findOne({
+//       where:{
+//         title: recipeTitle
+//       }
+//     }).then(function(recipe){
+//       //recipe is the result of my query
+//       if(recipe === null){
+//         db.favorite.create({
+//           title: recipeTitle
+//         }).then(function(recipe){
+//           //here poke is what db returned from create
+//           console.log('created ', recipe.title);
+//           res.redirect('/favorite/show');
+//         });
+//       }else{
+//         console.log('already added: ', recipe.title);
+//         res.redirect('/recipe');
+//       }
+//     });
+//
+// });
 
-    var recipeTitle = req.body.title;
-    db.favorite.findOne({
-      where:{
-        title: recipeTitle
-      }
-    }).then(function(recipe){
-      //recipe is the result of my query
-      if(recipe === null){
-        db.favorite.create({
-          title: recipeTitle
-        }).then(function(recipe){
-          //here poke is what db returned from create
-          console.log('created ', recipe.title);
-          res.redirect('/favorite/show');
-        });
-      }else{
-        console.log('already added: ', recipe.title);
-        res.redirect('/recipe');
-      }
+// POST /projects - create a new project
+router.post('/favorite/show', function(req, res) {
+    db.recipe.create({
+        title: req.body.title,
+        url: req.body.href,
+        ingredient: req.body.ingredients
+    }).then(function(newProject) {
+        var recipe = [];
+        if (req.body.food) {
+            recipes = req.body.food.split(",");
+        }
+        if (categories.length > 0) {
+            async.forEachSeries(recipes, function(recipe, callback) {
+                //functions that runs for each category
+
+                // 2. insert one or many categories for this one project
+                db.food.findOrCreate({
+                    where: { name: food.trim() }
+                }).spread(function(recipe, wasCreated) {
+                    // add the info into the join table
+
+                    // 3. populating the join table for each category for that project
+                    newFavorite.addRecipe(recipe);
+                    callback();
+                });
+            }, function() {
+                //runs when everything is done
+                res.redirect("/favorite/show");
+            });
+        } else {
+            res.redirect("/recipes");
+        }
+    }).catch(function(error) {
+        res.status(400).render('main/404');
     });
-
 });
 
 module.exports = router;
